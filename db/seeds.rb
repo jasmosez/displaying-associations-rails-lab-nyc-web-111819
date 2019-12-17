5f5e9d8bc44c82b8a1db6a465c91d0b18880d42c
jasmosez <jas.schaffer@gmail.com>
2019-12-17 18:36:17 -0500

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+hc = Artist.create(name: "Hot Chip")
+msb = Artist.create(name: "Menahan Street Band")
+bd = Artist.create(name: "Bob Dylan")
+
+Song.create(title: "One Life Stand", artist_id: hc.id)
+Song.create(title: "No God", artist_id: hc.id)
+Song.create(title: "Ready for the Floor", artist_id: hc.id)
+Song.create(title: "Make the Road by Walking", artist_id: msb.id)
+Song.create(title: "Tired of Fighting", artist_id: msb.id)
+Song.create(title: "Home Again", artist_id: msb.id)
+Song.create(title: "Don't Think Twice", artist_id: bd.id)
+Song.create(title: "Hurricane", artist_id: bd.id)
+Song.create(title: "Karina", artist_id: msb.id)
